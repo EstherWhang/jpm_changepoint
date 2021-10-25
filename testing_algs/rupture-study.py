36615ddfabcb2be21af49d927cddbb323fbcf36f
Esther Whang <44009604+EstherWhang@users.noreply.github.com>
2021-10-24 22:24:45 -0400

    added rupture-study

---
+++ testing_algs/rupture-study.py
@@ -0,0 +1,11 @@
+from modules.gener import *
+from modules.utilities import *
+
+
+#todo: make a bunch of synthetic time series datasets and test rupture algorithms on them
+#Binary Segmentation (https://github.com/deepcharles/ruptures/blob/master/docs/user-guide/detection/binseg.md)
+#Bottom-up segmentation (https://github.com/deepcharles/ruptures/blob/master/docs/user-guide/detection/bottomup.md)
+#Dynamic Programming (https://github.com/deepcharles/ruptures/blob/master/docs/user-guide/detection/dynp.md)
+#Kernel change point detection (https://github.com/deepcharles/ruptures/blob/master/docs/user-guide/detection/kernelcpd.md)
+#Linearly penalized segmentation (https://github.com/deepcharles/ruptures/blob/master/docs/user-guide/detection/pelt.md)
+#Window-based change point detection (https://github.com/deepcharles/ruptures/blob/master/docs/user-guide/detection/window.md)
