c33300dcb188e95d03d59f365a007d1cc9e6ef56
Esther Whang <44009604+EstherWhang@users.noreply.github.com>
2021-11-06 14:09:53 -0400

    fixes

--- real_data_test/plots_ruptures.py
+++ real_data_test/plots_ruptures.py
@@ -4,44 +4,48 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-def plot_company(company_stock, company_name, start, end):
-    df = pd.read_csv("company_stock")
+def plot_close(df, company_name, start, end):
     df_range = df.loc[(df['Date'] >= start) & (df['Date'] < end)]
     df_range.plot(x='Date',y='Close')
-    plt.title("company_name")
+    plt.title(company_name)
   
-def bin_seg(signal, dates, model, sigma):
+def bin_seg(signal, dates, model, sigma = 5):
     #model = "l2"  # "l1", "rbf", "linear", "normal", "ar",...
     algo = rpt.Binseg(model=model).fit(signal)
     sigma = 5 #std of noise
-    my_bkps = algo.predict(epsilon=3 * len(signal) * sigma ** 2)
+    bkps = algo.predict(epsilon=3 * len(signal) * sigma ** 2)
     # show results
-    print(dates[my_bkps[:-1]])
-    rpt.show.display(signal, my_bkps, figsize=(10, 6))
+    print("Binary Seg Breaks: "+ str(dates[bkps[:-1]]))
+    rpt.show.display(signal, bkps)
     plt.title("Binary Segmentation")
-    plt.show()
-def bottom_up(signal, dates, model, sigma ):
+    #plt.show()
+
+def bottom_up(signal, dates, model, sigma = 5):
   #model = "l2"  # "l1", "rbf", "linear", "normal", "ar",...
     algo = rpt.BottomUp(model=model).fit(signal)
-    sigma = 5 #std of noise
-    my_bkps = algo.predict(epsilon=3 * n * sigma ** 2)
+    bkps = algo.predict(epsilon=3 * len(signal) * sigma ** 2)
   # show results
-    print(dates[my_bkps[:-1]])
-    rpt.show.display(signal, my_bkps, figsize=(10, 6))
-    plt.title("Binary Segmentation")
-    plt.show()
+    print("Bottom Up Breaks: "+ str(dates[bkps[:-1]]))
+    rpt.show.display(signal, bkps)
+    plt.title("Bottom-up segmentation")
+    #plt.show()
+
 #models : "l2" linear, "rbf" gaussian
 #Linearly penalized segmentation
 #https://github.com/deepcharles/ruptures/blob/master/docs/examples/kernel-cpd-performance-comparison.ipynb
 def pelt(signal, dates, model, jump=1, min_size=2,beta=100 ): #alternative to dynamic, linear kernal
     algo = rpt.Pelt(model=model, jump=jump, min_size=min_size).fit(signal)
     bkps = algo.predict(pen=beta)
-    rpt.show.display(signal, bkps, figsize=(10, 6))
-    plt.show()
+    print("PELT: "+ str(dates[bkps[:-1]]))
+    rpt.show.display(signal, bkps)
+    plt.title("Linearly penalized segmentation")
+    #plt.show()
 
-def window(signal, dates, model):
+def window(signal, dates, model, sigma = 5):
     algo = rpt.Window(width=40, model=model).fit(signal)
-    my_bkps = algo.predict(epsilon=3 * n * sigma ** 2)
+    bkps = algo.predict(epsilon=3 * len(signal) * sigma ** 2)
     # show results
-    rpt.show.display(signal, my_bkps, figsize=(10, 6))
-    plt.show()
+    print("Window Breaks: "+ str(dates[bkps[:-1]]))
+    rpt.show.display(signal, bkps)
+    plt.title("Window-based change point detection")
+    #plt.show()
--- real_data_test/rupture_algs_jpm.py
+++ real_data_test/rupture_algs_jpm.py
@@ -3,21 +3,23 @@ import pandas as pd
 import ruptures as rpt
 import numpy as np
 import matplotlib.pyplot as plt
-
+from plots_ruptures import *
+#import data
 JPMorgan_df = pd.read_csv("https://github.com/EstherWhang/jpm_changepoint/raw/main/JPM.csv")
 print(JPMorgan_df.head())
-JPMorgan = JPMorgan_df[['Date', 'Close','Volume']]
-JPMorgan['Date'] = pd.to_datetime(JPMorgan['Date'], format='%Y-%m-%d')
-JPMorgan_recent = JPMorgan.loc[(JPMorgan['Date'] >= '2020-01-1')]
-JPMorgan_recent.plot(x='Date', y='Close')
-plt.title('Closing vs Date from 2020-01-01 to 2020-04-01')
-JPMorgan_recent.plot(x='Date', y='Volume')
-plt.title('Volume vs Date from 2020-01-01 to 2020-04-01')
-JPMorgan['Date'] = pd.to_datetime(JPMorgan['Date'], format='%Y-%m-%d')
-JPMorgan_2008 = JPMorgan.loc[(JPMorgan['Date'] >= '2006-01-1') & (JPMorgan['Date'] < '2010-01-01')]
-JPMorgan_2008.plot(x='Date', y='Close')
-JPMorgan_2008.plot(x='Date', y='Volume')
-JPMorgan['Date'] = pd.to_datetime(JPMorgan['Date'], format='%Y-%m-%d')
-JPMorgan_interesting = JPMorgan.loc[(JPMorgan['Date'] >= '2010-01-1') & (JPMorgan['Date'] < '2015-01-01')]
-JPMorgan_interesting.plot(x='Date', y='Close')
-JPMorgan_interesting.plot(x='Date', y='Volume')
+JPMorgan_df = JPMorgan_df[['Date', 'Close','Volume']]
+plot_close(JPMorgan_df, 'JP Morgan', '2020-01-01', '2020-04-01')
+
+#run it through rupture algorithms
+JPMorgan_recent = JPMorgan_df.loc[(JPMorgan_df['Date'] >= '2020-01-1')]
+signal = np.array(JPMorgan_recent['Close'])
+dates = np.array(JPMorgan_recent['Date'])
+model = "l2"
+
+bin_seg(signal, dates, model,sigma = 1)
+
+bottom_up(signal, dates, model, sigma = 1)
+
+pelt(signal, dates, model, jump=1, min_size=2,beta=100 )
+
+window(signal, dates, model, sigma = 1)
